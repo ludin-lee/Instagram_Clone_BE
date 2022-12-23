88d9f09f8382ce11a0639d333e78080a3d164124
chocheolhee <whcjfgml92@naver.com>
2022-12-24 01:09:11 +0900

    like routes authMiddleware

--- routers/like.router.js
+++ routers/like.router.js
@@ -5,6 +5,6 @@ const authMiddleware = require('../middlewares/auth');
 const LikeController = require('../controllers/like.controller');
 const likeController = new LikeController();
 
-router.post('/post/:postId', likeController.likePost);
+router.post('/post/:postId', authMiddleware, likeController.likePost);
 
 module.exports = router;
